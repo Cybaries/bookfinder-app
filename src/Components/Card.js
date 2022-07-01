2f266820561c2d72643b87b125b73ff48202fc87
Cybaries <amankantjha@gmail.com>
2022-07-01 23:47:44 +0530

    Bug fix

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,11 +5,10 @@ function Card({ book }) {
                 book.map((item, index) => {
                     let thumbnail = item.volumeInfo.imageLinks.smallThumbnail;
                     let title = item.volumeInfo.title;
-                    // let amount = item.volumeInfo.listPrice.amount && item.volumeInfo.listPrice;
                     return (
                         <>
                             <div className="card">
-                                <img src={thumbnail} alt="Logo" />
+                                <img src={thumbnail} alt="" />
                                 <div className="bottom">
                                     <h3 className="title flex-item">{title}</h3>
                                     <p className="amount flex-item">&#8377;350</p>
