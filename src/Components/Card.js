7f3030ba9a650526cbaeecca858d4ad1404be75b
Cybaries <amankantjha@gmail.com>
2022-07-02 21:08:54 +0530

    styled searchbar

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,15 +3,23 @@ function Card({ book }) {
         <>
             {
                 book.map((item, index) => {
+                    console.log(item)
                     let thumbnail = item.volumeInfo.imageLinks.smallThumbnail;
                     let title = item.volumeInfo.title;
+                    let amount;
+                    if (item.saleInfo.saleability === 'FOR_SALE') {
+                        amount = item.saleInfo.listPrice.amount + ' ' + item.saleInfo.listPrice.currencyCode;
+                    }
+                    else {
+                        amount = 'Not for sale';
+                    }
                     return (
                         <>
                             <div className="card">
                                 <img src={thumbnail} alt="" />
                                 <div className="bottom">
                                     <h3 className="title flex-item">{title}</h3>
-                                    <p className="amount flex-item">&#8377;350</p>
+                                    <p className="amount flex-item">{ amount }</p>
                                 </div>
                             </div>
                         </>
